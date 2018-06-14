66f560aec873fdb4736bcb78a60f18ad92227fa2
Inkswipe <inkswipe@inkswipes-mac-mini.local>
2018-06-14 10:29:44 +0530

    Initial Commit

---
+++ JSONParsingFromBundleSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParsingFromBundleSwift4
+//
+//  Created by Inkswipe on 6/14/18.
+//  Copyright © 2018 Fortune4 Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
